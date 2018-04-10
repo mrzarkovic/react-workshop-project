ac9868e79908551274b2d5b09acab2fd91aa3a32
BIT Student <classrom@bgit.rs>
2018-04-10 17:24:55 +0200

    create base App component

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Moja prva komponenta
+            </div>
+        );
+    }
+}
+
+export default App;
