d6f1b95331998ce86b63fbd9929ca89186087ac2
BIT Student <classrom@bgit.rs>
2018-04-13 09:31:51 +0200

    .

--- src/pages/UsersProfile.js
+++ src/pages/UsersProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class UsersProfile extends Component {
     render() {
+        console.log(this.props.location)
         return (
             <div className="user">
                 Users profile strana: {this.props.match.params.id}
