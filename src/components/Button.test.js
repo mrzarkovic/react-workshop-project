133f3f047eab58f31909e773d5c29e365672f25d
BIT Student <classrom@bgit.rs>
2018-04-13 12:04:48 +0200

    remove coverage dir from git

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -5,20 +5,27 @@ import Button from 'Components/Button';
 
 describe('Test case button', () => {
     it('Should render text', () => {
-        const wrapper = shallow(<Button text="Dodaj" />);
+        const wrapper = shallow(<Button text="Dodaj" handler={jest.fn()} />);
         expect(wrapper.find('button').props().children).toEqual('Dodaj');
     });
 
     it('Should call life-cycle method `shouldComponentUpdate`', () => {
         spyOn(Button.prototype, 'shouldComponentUpdate').and.callThrough();
-        const wrapper = shallow(<Button text="Dodaj" />);
+        const wrapper = shallow(<Button text="Dodaj" handler={jest.fn()} />);
         wrapper.setProps({text: 'Nesto drugo'});
         expect(Button.prototype.shouldComponentUpdate).toHaveBeenCalledTimes(1);
     });
 
     it('Should not re-render', () => {
-        const component = shallow(<Button text="Dodaj" />)
+        const component = shallow(<Button text="Dodaj" handler={jest.fn()} />)
         const shouldUpdate = component.instance().shouldComponentUpdate({text: 'Dodaj'}, null);
         expect(shouldUpdate).toBe(false)
+    });
+
+    it('Should call handler', () => {
+        const onClickFn = jest.fn();
+        const wrapper = shallow(<Button text="Dodaj" handler={onClickFn} />);
+        wrapper.find('button').simulate('click');
+        expect(onClickFn).toHaveBeenCalledTimes(1);
     });
 });
